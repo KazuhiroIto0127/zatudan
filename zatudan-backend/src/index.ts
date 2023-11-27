1a8749fee36b568ccae0943e936a290a03d98bfa
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2023-11-28 00:52:31 +0900

    [backend]cors追加

--- zatudan-backend/src/index.ts
+++ zatudan-backend/src/index.ts
@@ -1,7 +1,19 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { zatudan } from './zatudan/api'
 
 const app = new Hono()
+app.use(
+  '/api/*',
+  cors({
+    origin: ['https://zatudan-backend.kazuhiroito0127.workers.dev', 'http://localhost:3001'],
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+)
 
 app.route('/api/zatudan', zatudan)
 
