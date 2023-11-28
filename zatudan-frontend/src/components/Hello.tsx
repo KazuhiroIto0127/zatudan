0c52337bd978185b21a2d4906f89482ce86e6aed
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2023-11-28 12:47:45 +0900

    [frontend] alertをやめて下部にテキスト表示するように変更

--- zatudan-frontend/src/components/Hello.tsx
+++ zatudan-frontend/src/components/Hello.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Hello(){
+  const [text, setText] = useState(""); // 新しいstateを追加
+
   const handleClick = async () => {
     try{
       const apiUrl = "https://zatudan-backend.kazuhiroito0127.workers.dev";
       const url = `${apiUrl}/api/topics/random`;
       const response = await fetch(url);
       const data = await response.json();
-      alert(data[0].body);
+      setText(data[0].body);
     } catch (error) {
       if (error instanceof Error) {
         alert('エラーが発生しました: ' + error.message);
@@ -19,9 +21,12 @@ export default function Hello(){
   }
 
   return (
-    <button onClick={handleClick}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      話題を引く
-    </button>
+    <>
+      <button onClick={handleClick}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        話題を引く
+      </button>
+      {text && <p>{text}</p>}
+    </>
   )
 }
