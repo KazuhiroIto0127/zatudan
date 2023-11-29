3dcc4fcc13495225b1bd24a526191d57e883679d
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2023-11-29 20:59:20 +0900

    [frontend] typescript化した

--- zatudan-frontend/src/index.tsx
+++ zatudan-frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './components/Hello.tsx';
+import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Hello />
